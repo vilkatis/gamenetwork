9e2013e73dce9def5fa20e40fcd4ba8edbe47b1a
vilkatis <garbalau@gmail.com>
2018-07-22 10:12:51 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {LoginComponent} from './components/login/login.component';
+import {InputComponent} from './components/input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule
