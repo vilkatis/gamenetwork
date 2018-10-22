d4b49d9c5ccf04351d4ba465db069b2d4eb467ee
vilkatis <garbalau@gmail.com>
2018-10-23 01:23:30 +0300

    added fix for height on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  public ngOnInit() {
+    this.onResize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  public onResize() {
+    document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+  }
 }
