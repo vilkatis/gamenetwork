339feec4924fa886e3deae9dd6487dbbd2f2078c
vilkatis <garbalau@gmail.com>
2018-10-22 03:47:01 +0300

    changed server

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.get('/*', function (req, res) {
 
 // Start the app by listening on the default Heroku port
 app.listen(4200);
-console.log('listening on port');
+console.log('listening on port 4200');
 
