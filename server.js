86dcf69fbeaded99cfda556a483810e3748c29dc
vilkatis <garbalau@gmail.com>
2018-10-22 03:45:31 +0300

    fixed

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+var express = require('express');
+var path = require('path');
 
-const app = express();
+var app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/game-social-network'));
 
 app.get('/*', function (req, res) {
-
-  res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+  console.log('getting app');
+  res.sendFile(path.join(__dirname + '/dist/game-social-network/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(4200);
+console.log('listening on port');
+
